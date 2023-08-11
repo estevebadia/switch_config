a2430c9e80fa601956699f99e7e61c856c6f40e6
Esteve Badia <badia.orive@gmail.com>
2023-08-11 15:48:27 +0200

    Auth script for both Kaltura and core LTI

---
+++ auth.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Proxy auth script so both Kaltura plugin and core moodle can work with
+ * Kaltura LTI.
+ *
+ * This script checks the cmid property of the login_hint paramenter. If it is
+ * one of the constant values used by the Kaltura plugin, it delegates the
+ * request to the kaltura auth.php script, otherwise it delegates the request
+ * to the core lti module.
+ */
+
+require_once(__DIR__ . '/../../../../config.php');
+$loginhint = optional_param('login_hint', '', PARAM_TEXT);
+$kaltura_plugin = false;
+if (!empty($loginhint)) {
+  $kaltura_modules = ['mymedia', 'coursegallery', 'browseembed'];
+  $ltimessagehint = json_decode($loginhint);
+  $kaltura_plugin = in_array($ltimessagehint->cmid, $kaltura_modules);
+}
+
+if ($kaltura_plugin) {
+  include __DIR__ . '/../../../../local/kaltura/auth.php';
+} else {
+  include __DIR__ . '/../../auth.php';
+}
